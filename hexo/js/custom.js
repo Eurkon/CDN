b1a5d81f8e8de28fe0f34600ef10a893b11e9b3e
Eurkon <1124724692@qq.com>
2021-05-24 16:00:40 +0800

    fix: 增加trycatch统计图切换深浅模式

--- hexo/js/custom.js
+++ hexo/js/custom.js
@@ -153,28 +153,40 @@ function adjustFontSize (plus) {
 function switchPostChart () {
   // 这里为了统一颜色选取的是“明暗模式”下的两种字体颜色，也可以自己定义
   let color = document.documentElement.getAttribute('data-theme') === 'light' ? '#4c4948' : 'rgba(255,255,255,0.7)'
-  if (document.getElementById('posts-chart')) {
-    let postsOptionNew = postsOption
-    postsOptionNew.textStyle.color = color
-    postsOptionNew.title.textStyle.color = color
-    postsOptionNew.xAxis.axisLine.lineStyle.color = color
-    postsOptionNew.yAxis.axisLine.lineStyle.color = color
-    postsChart.setOption(postsOptionNew)
+  if (document.getElementById('posts-chart') && postsOption) {
+    try {
+      let postsOptionNew = postsOption
+      postsOptionNew.textStyle.color = color
+      postsOptionNew.title.textStyle.color = color
+      postsOptionNew.xAxis.axisLine.lineStyle.color = color
+      postsOptionNew.yAxis.axisLine.lineStyle.color = color
+      postsChart.setOption(postsOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
-  if (document.getElementById('tags-chart')) {
-    let tagsOptionNew = tagsOption
-    tagsOptionNew.textStyle.color = color
-    tagsOptionNew.title.textStyle.color = color
-    tagsOptionNew.xAxis.axisLine.lineStyle.color = color
-    tagsOptionNew.yAxis.axisLine.lineStyle.color = color
-    tagsChart.setOption(tagsOptionNew)
+  if (document.getElementById('tags-chart') && tagsOption) {
+    try {
+      let tagsOptionNew = tagsOption
+      tagsOptionNew.textStyle.color = color
+      tagsOptionNew.title.textStyle.color = color
+      tagsOptionNew.xAxis.axisLine.lineStyle.color = color
+      tagsOptionNew.yAxis.axisLine.lineStyle.color = color
+      tagsChart.setOption(tagsOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
-  if (document.getElementById('categories-chart')) {
-    let categoriesOptionNew = categoriesOption
-    categoriesOptionNew.textStyle.color = color
-    categoriesOptionNew.title.textStyle.color = color
-    categoriesOptionNew.legend.textStyle.color = color
-    categoriesChart.setOption(categoriesOptionNew)
+  if (document.getElementById('categories-chart') && categoriesOption) {
+    try {
+      let categoriesOptionNew = categoriesOption
+      categoriesOptionNew.textStyle.color = color
+      categoriesOptionNew.title.textStyle.color = color
+      categoriesOptionNew.legend.textStyle.color = color
+      categoriesChart.setOption(categoriesOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
@@ -182,25 +194,37 @@ function switchVisitChart () {
   // 这里为了统一颜色选取的是“明暗模式”下的两种字体颜色，也可以自己定义
   let color = document.documentElement.getAttribute('data-theme') === 'light' ? '#4c4948' : 'rgba(255,255,255,0.7)'
   if (document.getElementById('map-chart')) {
-    let mapOptionNew = mapOption
-    mapOptionNew.title.textStyle.color = color
-    mapOptionNew.visualMap.textStyle.color = color
-    mapChart.setOption(mapOptionNew)
+    try {
+      let mapOptionNew = mapOption
+      mapOptionNew.title.textStyle.color = color
+      mapOptionNew.visualMap.textStyle.color = color
+      mapChart.setOption(mapOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
   if (document.getElementById('trends-chart')) {
-    let trendsOptionNew = trendsOption
-    trendsOptionNew.title.textStyle.color = color
-    trendsOptionNew.xAxis.axisLine.lineStyle.color = color
-    trendsOptionNew.yAxis.axisLine.lineStyle.color = color
-    trendsOptionNew.textStyle.color = color
-    trendsChart.setOption(trendsOptionNew)
+    try {
+      let trendsOptionNew = trendsOption
+      trendsOptionNew.title.textStyle.color = color
+      trendsOptionNew.xAxis.axisLine.lineStyle.color = color
+      trendsOptionNew.yAxis.axisLine.lineStyle.color = color
+      trendsOptionNew.textStyle.color = color
+      trendsChart.setOption(trendsOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
   if (document.getElementById('sources-chart')) {
-    let sourcesOptionNew = sourcesOption
-    sourcesOptionNew.title.textStyle.color = color
-    sourcesOptionNew.legend.textStyle.color = color
-    sourcesOptionNew.textStyle.color = color
-    sourcesChart.setOption(sourcesOptionNew)
+    try {
+      let sourcesOptionNew = sourcesOption
+      sourcesOptionNew.title.textStyle.color = color
+      sourcesOptionNew.legend.textStyle.color = color
+      sourcesOptionNew.textStyle.color = color
+      sourcesChart.setOption(sourcesOptionNew)
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
